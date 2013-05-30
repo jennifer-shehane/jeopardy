462f14f73895adc0d6d65338c2be0e8b9426b33f
Jennifer Shehane <shehane.jennifer@gmail.com>
2013-05-30 19:36:29 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jeopardy::Application.config.secret_token = 'af9c039f8905bc713eba51f0c0b160a1cae8b60faed344f152232d4f2d472f13d77dea4050d81f1377498a5bfc643ba97eb386fdc75bd4ec4c6a63a4ef81cf31'
